f8bf5e3de92c5eb0d93b8638190941819bfb42d5
Jessiah Lucien <jessiah29@gmail.com>
2018-11-08 23:25:41 +0000

    yur

--- js/script.js
+++ js/script.js
@@ -5,6 +5,25 @@
 //GLOBAL VARIABLES
 /* global $ */
 
+$("#shoot").click(function() {
+    $("#userChoice").text($("#input").val());
+});
+
+$("#shoot").click(function() {
+    var computerChoice = Math.random();
+console.log(computerChoice);
+if (computerChoice <= 0.33) {
+computerChoice = "Rock";
+} else if (computerChoice <=0.66) {
+computerChoice = "Paper";
+} else {
+computerChoice = "Scissors";
+}
+$("#computerChoice").text($("#input").val());
+});
+
+
+
 
 
 // DOCUMENT READY FUNCTION BELOW
